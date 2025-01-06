61c9765bb83394f0be92c12963250824eebcb751
Florian Kleber <kleber@snek.at>
2025-01-06 01:08:44 +0000

    feat: add google maps to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,12 @@ const IndexPage: React.FC<PageProps> = () => {
     <>
       <Hero />
       {/* <ClientsMarquee w="full" /> */}
+      <GoogleMaps
+        objectFit="cover"
+        h="full"
+        w="100%"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2740.0340435405824!2d14.295087575992476!3d46.626089555171056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47707481801f6b21%3A0x8399759ad0778c8f!2sSterneckstra%C3%9Fe%2023%2F20%2C%209020%20Klagenfurt%20am%20W%C3%B6rthersee!5e0!3m2!1sen!2sat!4v1718256423376!5m2!1sen!2sat"
+      />
     </>
   );
 };
