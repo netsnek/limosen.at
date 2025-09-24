c9d2deb559d73de6809335909e43536b9ff078b3
Florian Kleber <kleber@snek.at>
2025-09-24 17:15:18 +0200

    chore: update repo and redirect url

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -22,7 +22,7 @@ const config: GatsbyConfig = {
       options: {
         pylonUrl: 'https://services.netsnek.com/jaen/graphql',
         remote: {
-          repository: 'netsnek/nadine-hauswirth'
+          repository: 'netsnek/limosen.at'
         },
         zitadel: {
           organizationId: '339284789469124181',
@@ -30,7 +30,7 @@ const config: GatsbyConfig = {
           authority: 'https://accounts.netsnek.com',
           redirectUri:
             process.env.NODE_ENV === 'production'
-              ? 'https://limosen.at'
+              ? 'https://new.limosen.at/'
               : 'https://psychic-dollop-6vwv6x9vq9jf464g-8000.app.github.dev',
           projectIds: [
             '2268283277977065078'
